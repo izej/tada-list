abfe075e1a322efed737dba2fbb6c0616d4b6db1
Izabela Bożęcka <izabela.bozecka@gmail.com>
2025-05-13 12:41:51 +0200

    fixed typo

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const API_BASE = 'http://localhost:8080/api/v1/auth';
 
-interface Credencials {
+interface Credentials {
 email: string;
 password: string;
 }
 
-export const registerUser = async (userData: Credencials) => {
+export const registerUser = async (userData: Credentials) => {
   const res = await axios.post(`${API_BASE}/signup`, userData);
   return res.data;
 };
 
-export const loginUser = async (credentials: Credencials) => {
+export const loginUser = async (credentials: Credentials) => {
   const res = await axios.post(`${API_BASE}/login`, credentials);
   return res.data;
 };
