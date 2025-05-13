f258b3f0052d95ef19f1ea7a548395606049ea20
Izabela Bożęcka <izabela.bozecka@gmail.com>
2025-05-13 12:42:04 +0200

    improvements

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 const API_BASE = 'http://localhost:8080/api/v1/auth';
 
 interface Credentials {
-email: string;
-password: string;
+  email: string;
+  password: string;
 }
 
 export const registerUser = async (userData: Credentials) => {
